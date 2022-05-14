9d658fdca2069ae5f6941c1cce3eb490df9a9641
ThangVPham <thangpham1412@gmail.com>
2022-05-14 12:52:43 -0400

    added weather background

--- script/typewriterEffect.js
+++ script/typewriterEffect.js
@@ -10,11 +10,11 @@ let typewriter = new Typewriter(typewriterSection, {
 
     typewriter
     .pauseFor(1000)
-    .typeString("I Am A Creative Thinker.")
+    .typeString("I Am A <strong><span style='color: #c58512;'>Tech Enthusiast.</span></strong>")
     .pauseFor(2000)
-    .deleteChars(17) 
-    .typeString("Problem Solver.")
+    .deleteChars(16) 
+    .typeString("<strong><span style='color: #c58512;'>Problem Solver.</span></strong>")
     .pauseFor(2000)
     .deleteChars(15)
-    .typeString("Web Developer.")
+    .typeString("<strong><span style='color: #c58512;'>Software Developer.</span></strong>")
     .start();
