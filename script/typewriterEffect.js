badeafcd965475eba6b8274671a7affd1e574236
ThangVPham <thangpham1412@gmail.com>
2022-06-06 18:13:09 -0400

    typewritter speed increase

--- script/typewriterEffect.js
+++ script/typewriterEffect.js
@@ -2,16 +2,17 @@ let typewriterSection = document.querySelector('.typewriterSection');
 
 let typewriter = new Typewriter(typewriterSection, {
     loop: false,
-    delay: 75,
+    delay: 40,
+    deleteSpeed: 10,
   });
 
     typewriter
-    .pauseFor(1000)
+    .pauseFor(200)
     .typeString("I Am A <strong><span style='color: #c58512;'>Tech Enthusiast.</span></strong>")
-    .pauseFor(2000)
+    .pauseFor(1000)
     .deleteChars(16) 
     .typeString("<strong><span style='color: #c58512;'>Problem Solver.</span></strong>")
-    .pauseFor(2000)
+    .pauseFor(1000)
     .deleteChars(15)
     .typeString("<strong><span style='color: #c58512;'>Software Developer.</span></strong>")
     .start();
