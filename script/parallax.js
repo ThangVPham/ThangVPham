45c20e0a78cca5528d3cb2c1e920f1fee87a7ac9
Thang Pham <thangpham1412@gmail.com>
2022-09-27 13:09:56 -0400

    parallax scroll speed changed

--- script/parallax.js
+++ script/parallax.js
@@ -1,5 +1,5 @@
 const parallax = document.getElementById("parallax");
 window.addEventListener("scroll", () => {
   let offset = window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.4 + "px";
+  parallax.style.backgroundPositionY = offset * 0.8 + "px";
 });
