4fb840f2025c8a955c0d620cec0db19ca0ac45e6
Thang Pham <thangpham1412@gmail.com>
2023-04-04 12:19:11 -0400

    fixed typewriter effect bug

--- script/typewriterEffect.js
+++ script/typewriterEffect.js
@@ -1,18 +1,24 @@
-let typewriterSection = document.querySelector('.typewriterSection');
+let typewriterSection = document.querySelector(".typewriterSection");
 
 let typewriter = new Typewriter(typewriterSection, {
-    loop: false,
-    delay: 40,
-    deleteSpeed: 10,
-  });
+  loop: false,
+  delay: 40,
+  deleteSpeed: 10,
+});
 
-    typewriter
-    .pauseFor(200)
-    .typeString("I Am A <strong><span style='color: #c58512;'>Tech Enthusiast.</span></strong>")
-    .pauseFor(1000)
-    .deleteChars(16) 
-    .typeString("<strong><span style='color: #c58512;'>Problem Solver.</span></strong>")
-    .pauseFor(1000)
-    .deleteChars(15)
-    .typeString("<strong><span style='color: #c58512;'>Web Developer.</span></strong>")
-    .start();
+typewriter
+  .pauseFor(200)
+  .typeString(
+    "I Am A <strong><span style='color: #c58512;'>Tech Enthusiast.</span></strong>"
+  )
+  .pauseFor(1000)
+  .deleteChars(16)
+  .typeString(
+    "<strong><span style='color: #c58512;'>Problem Solver.</span></strong>"
+  )
+  .pauseFor(1000)
+  .deleteChars(15)
+  .typeString(
+    "<strong><span style='color: #c58512;'>Web Developer.</span></strong>"
+  )
+  .start();
